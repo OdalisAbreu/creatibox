6111eb5fe9933b7b0479c8c526dd155df1c3bf82
OdalisAbreu <odalisdabreu@gmail.com>
2026-03-08 10:28:13 -0400

    Update message

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,9 +29,7 @@ class DatabaseSeeder extends Seeder
             'phone' => '18299387179',
             'token' => '339512|rZkXM1LIG2GlgZmKZ5EMTK32HHMuOJTrvEKGB0Tgcebcd968',
             'wasapi_id' => '20349',
-            'final_message' => '¡Listo! Tu factura ha sido recibida correctamente. ✅
-
-*
+            'final_message' => '.
 
 ¡Gracias por participar en Arma tu Combo y Gana!
 
@@ -40,8 +38,6 @@ Recuerda: Mientras más compres, ¡más chances tienes de ganar!
 Promoción válida hasta el 3 de abril 2026.
 
 ¡Mucha suerte!
-
-¡Mucha suerte! 🍀
 ',
         ]);
     }
